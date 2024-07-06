947e02704817b4a3230d837b84531dcd35f474da
Mohammad Mahdi Gholami <gholamimahdi680@gmail.com>
2024-07-06 15:12:32 +0330

    Destroy Obstacle with Effect

--- Assets/Scripts/BulletCollision.cs
+++ Assets/Scripts/BulletCollision.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BulletCollision : MonoBehaviour
 {
     private int obstacleHealth;
     [SerializeField] private ParticleSystem hitEffect;
+    private float destroyPlyerDelay = 0.1f;
+
     private void OnTriggerEnter2D(Collider2D other) {
         obstacleHealth = gameObject.GetComponent<ObstacleHealthBarController>().Health;
         if (other.tag == "Bullet" && obstacleHealth <= 1){
-            //hitEffect.Play();
-            gameObject.SetActive(false);
+            hitEffect.Play();
+            Invoke("DestroyPlayer",destroyPlyerDelay);
         } else {
             gameObject.GetComponent<ObstacleHealthBarController>().Health -= 1;
         }
@@ -18,6 +21,10 @@ public class BulletCollision : MonoBehaviour
         if (other.tag == "Bullet"){
             other.gameObject.SetActive(false);
         }
+    }
+    
+    void DestroyPlayer(){
+        gameObject.SetActive(false);
     }
         
 }
