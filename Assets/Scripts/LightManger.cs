f2fdbf218b549fb8bee4eb4ce10a10ac51afd0a3
Mohammad Mahdi Gholami <gholamimahdi680@gmail.com>
2024-07-14 14:38:36 +0330

    Add Dawn

--- Assets/Scripts/LightManger.cs
+++ Assets/Scripts/LightManger.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
@@ -5,22 +6,36 @@ public class LightManger : MonoBehaviour
 {
     [SerializeField] Light2D rightLight;
     [SerializeField] Light2D leftLight;
-    [SerializeField] Light2D GameLight;
+    [SerializeField] Light2D gameLight;
+    [SerializeField] Light2D sun;
     private readonly float minGameLight = 0;
     private readonly float maxCarLight = 2;
+    private bool changeSun = true;
     private float speedMultiplier = 0;
+    private float sunMultiplier = 0;
     private void Start() {
-        GameLight.intensity = 1;
+        sun.enabled = true;
+        gameLight.intensity = 1;
         rightLight.intensity = 0;
         leftLight.intensity = 0;
+        sun.intensity = 0;
     } 
     private void Update() {
         speedMultiplier += Time.deltaTime * 0.0000009f;
-        if (GameLight.intensity > minGameLight && rightLight.intensity <= maxCarLight){
-            GameLight.intensity -= speedMultiplier;
+        if (gameLight.intensity > 0.5f) {   
+            sunMultiplier += Time.deltaTime * 0.0000009f;
+        } else {
+            sunMultiplier -= Time.deltaTime * 0.0000009f;
+        }
+        if (gameLight.intensity > minGameLight && rightLight.intensity <= maxCarLight){
+            gameLight.intensity -= speedMultiplier;
             leftLight.intensity += speedMultiplier * 2;
             rightLight.intensity += speedMultiplier * 2 ;
+            sun.intensity += sunMultiplier * 200;
+            if (gameLight.intensity < 0.5f && changeSun) {
+                changeSun = false;
+                sunMultiplier = -sunMultiplier;   
+            }
         }
     }
-
 }
