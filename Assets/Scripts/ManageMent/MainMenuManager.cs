4e32cda8ffa9840150cb873af2f15fddf25a0329
Mohammad Mahdi Gholami <gholamimahdi680@gmail.com>
2024-07-23 14:37:31 +0330

    Typo & Folder Change

--- Assets/Scripts/ManageMent/MainMenuManager.cs
+++ Assets/Scripts/ManageMent/MainMenuManager.cs
@@ -1,12 +1,12 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
 using Nakama;
 using Nakama.TinyJson;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-
-public class LeaderBoardManager : MonoBehaviour
+public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI leaderBoardNames;
     [SerializeField] private TextMeshProUGUI leaderBoardScore;
@@ -21,8 +21,11 @@ public class LeaderBoardManager : MonoBehaviour
     private string deviceId;
     private ISession session;
 
-    async void Start()
-    {   
+    public void LoadScene(String sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+
+    async void Start() {   
         // myDeviceID = 514f368d37e872c3bc97aa11a3daf2180cb7bd14
         deviceId = SystemInfo.deviceUniqueIdentifier;
         leaderBoardNames.text = "";
@@ -32,27 +35,21 @@ public class LeaderBoardManager : MonoBehaviour
         GetLeaderBoard();
     }
 
-    private async void GetLeaderBoard()
-    {   
-
+    private async void GetLeaderBoard() {   
         var result = await client.RpcAsync(session, "get_bucket_records");
         var records = result.Payload.FromJson<Records>();
         for (int i = 0; i < records.records.Count; i++) {
             leaderBoardNames.text += records.records[i].username.value + "\n";
             leaderBoardScore.text += records.records[i].score + "\n";
         }
-
     }
 }
 
-
-public class Records
-{
+public class Records {
     public List<Record> records { get; set; }
 }
 
-public class Record
-{
+public class Record {
     public string leaderboard_id { get; set; }
     public string owner_id { get; set; }
     public Username username { get; set; }
@@ -66,22 +63,18 @@ public class Record
     public int max_num_score { get; set; }
 }
 
-public class Username
-{
+public class Username {
     public string value { get; set; }
 }
 
-public class CreateTime
-{
+public class CreateTime {
     public int seconds { get; set; }
 }
 
-public class UpdateTime
-{
+public class UpdateTime {
     public int seconds { get; set; }
 }
 
-public class ExpiryTime
-{
+public class ExpiryTime {
     public int seconds { get; set; }
 }
